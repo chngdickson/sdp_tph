e83a12ddcab7e0368eb89b98bbbd15e5bb8c3163
chngdickson <chngdickson@gmail.com>
2025-02-24 18:22:43 +0800

    test wiuth custom reconstruction

--- main/utility/diamNCrown.py
+++ main/utility/diamNCrown.py
@@ -15,10 +15,43 @@ class AdTree_cls():
         self.adTree_exe = Paths.get_adtree()
         
     def reconstruct_skeleton(self, tree_cloud):
-        labels = tree_utils.leafwood_classificiation(tree_cloud, method='surface_variation')
+        labels = self.leafwood_classificiation(tree_cloud, method='surface_variation')
         wood_cloud = tree_cloud.select_by_index(np.where(labels==Labels.WOOD)[0])
         skeleton = tree_utils.reconstruct_skeleton(wood_cloud, self.adTree_exe)
         tree_utils.show_tree(tree_cloud, labels, skeleton)
     
+    def surface_variation_filter(self, pcd, radius, threshold):
+        """Compute surface variation of point cloud."""
+        pcd.estimate_covariances(
+            search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=radius, max_nn=2))
+        eig_val, _ = np.linalg.eig(np.asarray(pcd.covariances))
+        eig_val = np.sort(eig_val, axis=1)
+        sv = eig_val[:,0] / eig_val.sum(axis=1)
+        mask = sv < threshold
+        return mask
+    
+    def leafwood_classificiation(self, tree_cloud, method):
+        """Leaf-wood classification."""
+
+        labels = np.full(len(tree_cloud.points), Labels.LEAF, dtype=int)
+
+        # outlier removal
+        pcd_, _, trace = tree_cloud.voxel_down_sample_and_trace(0.02,
+                                        tree_cloud.get_min_bound(),
+                                        tree_cloud.get_max_bound())
+        pcd_, ind_ = pcd_.remove_statistical_outlier(nb_neighbors=16, std_ratio=2.0)
+        ind_ = np.asarray(ind_)
+
+        # classify
+        if method == 'curvature':
+            mask = o3d_utils.curvature_filter(pcd_, .075, min1=20, min2=35)
+            ind = np.hstack([trace[i] for i in ind_[mask]])
+        else:
+            mask = self.surface_variation_filter(pcd_, .075, .15)
+            ind = np.hstack([trace[i] for i in ind_[mask]])
+
+        labels[ind] = Labels.WOOD
+
+        return labels
     def segment_tree(self, tree_cloud):
         pass
