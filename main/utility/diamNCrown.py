9da2511a1448d2af092efda17fc8e2833293fd2a
chngdickson <chngdickson@gmail.com>
2025-02-24 18:02:24 +0800

    check if changes has worked

--- main/utility/diamNCrown.py
+++ main/utility/diamNCrown.py
@@ -3,7 +3,7 @@ sys.path.insert(1, '/root/sdp_tph/submodules/PCTM/pctm/src')
 
 import numpy as np
 import open3d as o3d
-import utils.tree_utils as tree_utils
+import adtreeutils.utils.tree_utils as tree_utils
 import adtreeutils.o3d_utils as o3d_utils
 from labels import Labels
 from config import Paths
