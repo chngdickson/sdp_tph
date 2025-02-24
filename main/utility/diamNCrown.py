c7f8384ec0aeb325d30e9be59ed4088147f0569e
chngdickson <chngdickson@gmail.com>
2025-02-24 18:03:07 +0800

    changes to name

--- main/utility/diamNCrown.py
+++ main/utility/diamNCrown.py
@@ -3,8 +3,8 @@ sys.path.insert(1, '/root/sdp_tph/submodules/PCTM/pctm/src')
 
 import numpy as np
 import open3d as o3d
-import adtreeutils.utils.tree_utils as tree_utils
-import adtreeutils.o3d_utils as o3d_utils
+import adTreeutils.utils.tree_utils as tree_utils
+import adTreeutils.o3d_utils as o3d_utils
 from labels import Labels
 from config import Paths
 adTree_exe = Paths.get_adtree()
