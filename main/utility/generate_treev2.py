07aa592a60040b1b346832af65a2de28bde1e887
chngdickson <chngdickson@gmail.com>
2025-02-24 14:25:06 +0800

    fefwef

--- main/utility/generate_treev2.py
+++ main/utility/generate_treev2.py
@@ -50,6 +50,8 @@ def find_centroid_from_Trees(grd_pcd, coord:tuple, radius_expand:int=3, zminmax:
         rigidness=2,
         iterations=1000
     )
+    if non_grd.is_empty():
+        return None
     xyz = np.asarray(non_grd.points)
     z_min_non_gnd = xyz[:,2].min()
     non_grd = non_grd.select_by_index(np.where(xyz[:,2]<z_min_non_gnd+2)[0])
