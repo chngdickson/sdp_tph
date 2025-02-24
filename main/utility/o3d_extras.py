4defffa649e40a8720d4803bb7f39c02e9a27309
chngdickson <chngdickson@gmail.com>
2025-02-24 20:53:44 +0800

    love of god

--- main/utility/o3d_extras.py
+++ main/utility/o3d_extras.py
@@ -16,8 +16,4 @@ def dbscan(tree_cloud, eps=0.5, min_points=10):
     colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
     colors[labels < 0] = 0
     tree_cloud.colors = o3d.utility.Vector3dVector(colors[:, :3])
-    o3d.visualization.draw_geometries([tree_cloud],
-                                    zoom=0.455,
-                                    front=[-0.4999, -0.1659, -0.8499],
-                                    lookat=[2.1813, 2.0619, 2.0999],
-                                    up=[0.1204, -0.9852, 0.1215])
+    o3d.visualization.draw_geometries([tree_cloud])
