dea39070069e3e18656dcccae48e2ba75a25303e
chngdickson <chngdickson@gmail.com>
2025-02-24 14:59:20 +0800

    fwefwefsdf

--- main/utility/generate_treev2.py
+++ main/utility/generate_treev2.py
@@ -61,7 +61,7 @@ def find_centroid_from_Trees(grd_pcd, coord:tuple, radius_expand:int=3, zminmax:
     if not xyz.size:
         return None
     else:
-        centroid, label_ = kmeans(xyz[:,0:2],k=1)
+        centroid, label_ = kmeans2(xyz[:,0:2],k=1)
         xnew,ynew = centroid[0]
         print(f"is finite {np.isfinite(xyz.all())}")
 
